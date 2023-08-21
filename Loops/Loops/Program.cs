af5a533488af12ee831dc55214c8e69bd14651fb
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-21 13:15:51 -0400

    while and do while

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -10,6 +10,81 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed " + number + "! try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed " + number +"! try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine(number + "  is correct!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You guessed " + number + ", you are wrong, try again");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            Console.Read();
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Guess a second number?");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed2 = false;
+
+            do
+            {
+                switch (number2)
+                {
+                    case 44:
+                        Console.WriteLine("You guessed 44, try again");
+                        Console.WriteLine("Guess a number?");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 33:
+                        Console.WriteLine("You guessed 33, try again");
+                        Console.WriteLine("Guess a number?");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 66:
+                        Console.WriteLine("66 is correct!");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        isGuessed2 = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong, try again");
+                        Console.WriteLine("Guess a number?");
+                        number2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed2);
+
+            Console.ReadLine();
+
         }
     }
 }
