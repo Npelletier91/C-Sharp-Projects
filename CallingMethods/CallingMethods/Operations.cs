084a573e9561fe1c8f1a41e6c5f1c63beeb5c449
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-24 15:24:44 -0400

    first push

---
+++ CallingMethods/CallingMethods/Operations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Operations
+    {
+        public int AddTen(int number)
+        {
+            return number + 5;
+        }
+        public int Square(int number)
+        {
+            return number * number;
+        }
+        public int Triple(int number)
+        {
+            return number * 3;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose a number to do math operations on:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            Operations operation = new Operations();
+
+            int result1 = operation.AddTen(userInput);
+            int result2 = operation.Square(userInput);
+            int result3 = operation.Triple(userInput);
+
+            Console.WriteLine(userInput + " + 10 = " + result1);
+            Console.WriteLine(userInput + "^2 = " + result2);
+            Console.WriteLine(userInput + " * 3 = " + result3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
