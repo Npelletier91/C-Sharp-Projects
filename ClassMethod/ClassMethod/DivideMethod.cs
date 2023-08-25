ca527c1c8a8d46d9fcf4001573cbd2b193054012
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-25 14:28:10 -0400

    first push

---
+++ ClassMethod/ClassMethod/DivideMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    public class DivideMethod
+    {
+        public void divideOperation(int number1, out int sum)
+        {
+            sum = number1 / 2;
+        }
+        public void divideOperation(Single number1, Single number2)
+        {
+            Single result = number1 + number2;
+            Console.WriteLine($"{number1} + {number2} = {result}");
+        }
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating an instance of the class to use in program
+            DivideMethod math = new DivideMethod();
+
+            //taking user input to use for math operation
+            Console.WriteLine("Enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            //setting variable to use when calling the "out" of the method.
+            int sum;
+
+            //calling the method with user input
+            math.divideOperation(userInput, out sum);
+
+            //printing the method with the "out" value included
+            Console.WriteLine($"{userInput} / 2 = {sum}");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Enter a number:");
+            Single userInput2 = Convert.ToSingle(Console.ReadLine());
+
+            //calling the overloaded method with two parameters as "floats"
+            math.divideOperation(userInput2, 432);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Enter one number at a time to subtract them");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //printing the static method to the console without creating an instance of the class "DivideMethod"
+            Console.WriteLine($"{num1} - {num2} = {DivideMethod.Subtract(num1, num2)}");
+            Console.ReadLine();
+
+        }
+    }
+}
