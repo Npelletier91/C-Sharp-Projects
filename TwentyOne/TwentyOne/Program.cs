1ce5164a945573c8380c727100fb37c3b21dcd97
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-25 19:05:08 -0400

    saved files

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -12,29 +12,16 @@ namespace TwentyOne
         {
 
             Deck deck = new Deck();
-            deck = Shuffle(deck);
+            int timesShuffled = 0;
+            deck.Shuffle(out timesShuffled, 4); //calling shuffle method (4) times. returning shuffled deck to deck.
             
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times Shuffled: {0}", timesShuffled);
             Console.ReadLine();
-        }
-
-        public static Deck Shuffle(Deck deck)
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while (deck.Cards.Count > 0) 
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
         }
     }
 }
