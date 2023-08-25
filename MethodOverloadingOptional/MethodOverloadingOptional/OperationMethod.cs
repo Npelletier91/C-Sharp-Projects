e87b6a235efec6242e93f1772c475b9e7a357d55
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-25 12:36:15 -0400

    first push

---
+++ MethodOverloadingOptional/MethodOverloadingOptional/OperationMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingOptional
+{
+    public class OperationMethod
+    {
+        
+        public int operation (int number1, int number2 = 0)
+        {
+            return number1 + number2;
+        }
+    }
+}
---
+++ MethodOverloadingOptional/MethodOverloadingOptional/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingOptional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationMethod math = new OperationMethod();
+
+
+            Console.WriteLine("Input a number:");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input another number or simply press enter:");
+            string stringInput = Console.ReadLine();
+            int userInput2 = string.IsNullOrEmpty(stringInput) ? 0 : Convert.ToInt32(stringInput);
+
+
+            int result = math.operation(userInput1,userInput2);
+
+            Console.WriteLine(userInput1 + " + " + userInput2 + " = " + result);
+            Console.ReadLine();
+        }
+    }
+}
