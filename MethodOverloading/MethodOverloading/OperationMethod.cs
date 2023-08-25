6b5a74b5eaf694c2236e3b97ce9bd4e8c5545505
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-25 10:17:17 -0400

    first push

---
+++ MethodOverloading/MethodOverloading/OperationMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class OperationMethod
+    {
+        Random random = new Random();
+        public int RandomNumber { get; private set; }
+
+        public int operation (int number)
+        {
+            return number - 5;
+        }
+        public int operation (Decimal number)
+        {
+            return Convert.ToInt32(number) * 1000;
+        }
+        public int operation (String number)
+        {
+            int randomNum = random.Next(0, 1000);
+            RandomNumber = randomNum;
+            return Convert.ToInt32(number) * randomNum;
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperationMethod math = new OperationMethod();
+
+            int integer = 5;
+
+            int result1 = math.operation(integer);
+            Console.WriteLine(integer + " - 5 = " + result1);
+            Console.ReadLine();
+
+            int result2 = math.operation(Convert.ToDecimal(integer));
+            Console.WriteLine(integer + " * 1000 = " + result2);
+            Console.ReadLine();
+
+            int result3 = math.operation(Convert.ToString(integer));
+            Console.WriteLine(integer + " * " + math.RandomNumber + " = " + result3);
+            Console.ReadLine();
+
+        }
+    }
+}
