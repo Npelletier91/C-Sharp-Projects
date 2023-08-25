8996576c380ec12867945cc2577e33e42010f7a9
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-25 12:55:33 -0400

    first push

---
+++ VoidMethod/VoidMethod/MathOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class MathOperation
+    {
+        public void operation (int number1, int number2)
+        {
+            int result = number1 * 10;
+            Console.WriteLine(number1 + " * 10 = " + result);
+            Console.WriteLine("The second integer parameter is: " + number2 + "\n");
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation math = new MathOperation();
+
+            math.operation(6, 7);
+
+            int example1 = 123;
+            int example2 = 456;
+
+            math.operation(example1, example2);
+
+            Console.ReadLine();
+        }
+    }
+}
