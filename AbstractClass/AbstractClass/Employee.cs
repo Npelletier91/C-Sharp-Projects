c8db77fb6e74c59377d2bfc4355994a7b4e8a3ed
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-26 11:25:12 -0400

    first push

---
+++ AbstractClass/AbstractClass/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+            Console.WriteLine($"ID: {ID}");
+        }
+        
+        public int ID { get; set; }
+        
+    
+    }
+}
