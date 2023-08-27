a7e0babba0a925697749398666a5323d1f101554
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-27 09:18:23 -0400

    genaric parameters added

--- AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/Employee.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    public class Employee : Person, IQuittable
+    public class Employee<T> : Person, IQuittable
     {
         public override void SayName()
         {
             base.SayName();
             Console.WriteLine($"ID: {ID}");
         }
-        
+        public List<T> things { get; set; }
         public int ID { get; set; }
         
         public void Quit()
@@ -21,13 +21,13 @@ namespace AbstractClass
             Console.WriteLine($"{FirstName} {LastName} has quit.");
         }
 
-        public static bool operator ==(Employee employee, Employee employee2)
-        {
-            return employee?.ID == employee2?.ID;  
-        }
-        public static bool operator !=(Employee employee, Employee employee2)
-        {
-            return !(employee == employee2);
-        }
+        //public static bool operator ==(Employee employee, Employee employee2)
+        //{
+        //    return employee?.ID == employee2?.ID;  
+        //}
+        //public static bool operator !=(Employee employee, Employee employee2)
+        //{
+        //    return !(employee == employee2);
+        //}
     }
 }
--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -10,23 +10,38 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", ID = 1001 };
-            Employee employee2 = new Employee() { FirstName = "Sample2", LastName = "Student2", ID = 1002 };
+            //Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", ID = 1001 };
+            //Employee employee2 = new Employee() { FirstName = "Sample2", LastName = "Student2", ID = 1002 };
 
-            employee.SayName();
+            //employee.SayName();
 
-            if (employee == employee2)
+            //if (employee == employee2)
+            //{
+            //    Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have the same ID");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have different IDs");
+            //}
+
+            //IQuittable quittableEmployee = employee;
+            //quittableEmployee.Quit();
+
+            Employee<string> Things = new Employee<string>();
+            Things.things = new List<string>() { "Shoe", "Hat", "Scarf"};
+            Employee<int> Stuff = new Employee<int>();
+            Stuff.things = new List<int>() { 2, 5, 7, 8 };
+
+            foreach (string thing in Things.things)
             {
-                Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have the same ID");
+                Console.WriteLine(thing);
             }
-            else
+
+            foreach (int thing in Stuff.things)
             {
-                Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have different IDs");
+                Console.WriteLine(thing);
             }
-
-            IQuittable quittableEmployee = employee;
-            quittableEmployee.Quit();
-
+            
             Console.ReadLine();
 
 
