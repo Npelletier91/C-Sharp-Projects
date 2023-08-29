3572308871aa52d71257b27fc4e1ec3151bc1a94
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-08-29 10:39:18 -0400

    fixed the commented code

---
+++ AbstractClass2/AbstractClass2/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass2
+{
+    public class Employee : Person
+    {
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            return employee?.ID == employee2?.ID;
+        }
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return !(employee == employee2);
+        }
+    }
+}
---
+++ AbstractClass2/AbstractClass2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student", ID = 1001 };
+            Employee employee2 = new Employee() { FirstName = "Sample2", LastName = "Student2", ID = 1002 };
+
+
+
+
+            if (employee == employee2)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have the same ID");
+            }
+            else
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} and {employee2.FirstName} {employee2.LastName} have different IDs");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
