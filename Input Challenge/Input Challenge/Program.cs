b1d0f6fc7c1645ecc5f88ecb5fe146ee8ab5d6e3
Npelletier91 <nicolas.pelletier@yahoo.com>
2023-09-03 13:19:48 -0400

    first push

---
+++ Input Challenge/Input Challenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Input_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DateTime.Now.ToString());
+
+            Console.WriteLine("Enter a number");
+            double number = Convert.ToInt32(Console.ReadLine());
+
+            DateTime newTime = (DateTime.Now.AddHours(number));
+
+            Console.WriteLine(newTime);
+            Console.ReadLine();
+
+
+
+            //DateTime yearOfBirth = new DateTime(1995, 5, 23, 8, 32, 45);
+            //DateTime yearOfGrad = new DateTime(2013, 6, 1, 16, 34, 22);
+
+            //TimeSpan ageAtGrad = yearOfGrad - yearOfBirth;
+
+
+            //Console.WriteLine("Enter a number");
+            //string number = Console.ReadLine();
+
+            //using (StreamWriter input = new StreamWriter(@"C:\Users\Nicol\Logs\log1.txt", false))
+            //{
+            //    input.WriteLine(number);
+            //}
+
+            //number = File.ReadAllText(@"C:\Users\Nicol\Logs\log1.txt");
+            //Console.WriteLine(number);
+            //Console.ReadLine();
+        }
+    }
+}
